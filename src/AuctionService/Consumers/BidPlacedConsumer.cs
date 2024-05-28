039ef5edf923418e91fff5ca49289365c57c1919
gorzki79 <marcin.gorzkiewicz@gmail.com>
2024-05-28 21:43:57 +0200

    End of section 6 - missing

--- src/AuctionService/Consumers/BidPlacedConsumer.cs
+++ src/AuctionService/Consumers/BidPlacedConsumer.cs
@@ -1,6 +1,31 @@
-﻿namespace AuctionService;
+﻿using AuctionService.Data;
+using Contracts;
+using MassTransit;
 
-public class BidPlacedConsumer
+namespace AuctionService;
+
+public class BidPlacedConsumer : IConsumer<BidPlaced>
 {
+    private readonly AuctionDbContext dbContext;
 
+    public BidPlacedConsumer(AuctionDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task Consume(ConsumeContext<BidPlaced> context)
+    {
+        Console.WriteLine("--> Consuming bid placed");
+
+        var auction = await dbContext.Auctions.FindAsync(Guid.Parse(context.Message.AuctionId));
+
+        if (auction.CurrentHighBid == null 
+            || context.Message.BidStatus.Contains("Accepted") 
+            && context.Message.Amount > auction.CurrentHighBid)
+        {
+            auction.CurrentHighBid = context.Message.Amount;
+            await dbContext.SaveChangesAsync();
+        }
+    }
 }
+
